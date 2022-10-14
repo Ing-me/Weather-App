881904986e8e2a78235318370c8820e6629be61b
Meide Djhymit <djhymitmeide27@gmail.com>
2022-10-14 16:00:43 -0400

    Edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 const path = require('path');
 
-module.exports ={
+module.exports = {
     entry: './src/index.js',
     output:{
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.(png|gif|jpg|jpeg|svg)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    }
+    mode: 'development'
 };
